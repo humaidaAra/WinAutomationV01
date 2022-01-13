847fbe8f8807713e161879180919c1d7c55ce6d3
Humaida Ara <57634386+humaidaAra@users.noreply.github.com>
2022-01-13 10:26:38 -0800

    Add files via upload

---
+++ WinAutomationV01/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace WinAutomationV01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 148);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(756, 104);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(613, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(652, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(801, 120);
+            this.label1.TabIndex = 1;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 264);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinAutomationV01/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics;
+using System.Media;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WinAutomationV01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             if(textBox1.Text.Equals(""))
+            {
+                SystemSounds.Beep.Play();
+                textBox1.Focus();
+                return;
+            }
+            try
+            {
+                String link = textBox1.Text;
+                String patt = @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$";
+                MatchCollection mat = Regex.Matches(link.Trim(), patt);
+                foreach (Match find in mat)
+                {
+                    Process p = new Process();
+                    string des = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                    // Redirect the output stream of the child process. 
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.Arguments = "/C yt-dlp -f bestvideo*+bestaudio/best -o \""+des+"\"\\vid \"" + link.Trim() + "\"";
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.RedirectStandardInput = true;
+                    p.Start();
+
+                    // Do not wait for the child process to exit before
+                    // reading to the end of it s redirected stream.
+
+                    String result = p.StandardOutput.ReadToEnd();
+
+                    p.WaitForExit();
+                    Process p2 = new Process();
+                    // Redirect the output stream of the child process. 
+                    p2.StartInfo.UseShellExecute = false;
+                    p2.StartInfo.CreateNoWindow = true;
+                    p2.StartInfo.FileName = "cmd.exe";
+                    p2.StartInfo.Arguments = "/C " + des + "\\vid.webm";
+                    p2.StartInfo.RedirectStandardOutput = true;
+                    p2.StartInfo.RedirectStandardInput = true;
+                    p2.Start();
+                    p2.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+        }
+    }
+}
